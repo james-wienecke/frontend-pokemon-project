451805d26477399c9d705b8b0bf2f2311a481ef3
james-wienecke <james.a.wienecke@gmail.com>
2022-04-04 18:27:39 -0500

    Began converting cypress specs to typescript.

--- cypress/integration/PokemonList.spec.tsx
+++ cypress/integration/PokemonList.spec.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import {mount} from '@cypress/react'
 import {PokemonList} from '../../components/PokemonList'
 
@@ -5,5 +6,10 @@ describe('PokemonList', () => {
     it('renders the pokemon list component', () => {
         mount(<PokemonList />)
         cy.get('#pokemon-list').should('exist')
+    })
+
+    it('displays at least one pokemon from the PokeAPI on component mount', () => {
+        mount(<PokemonList />)
+        cy.get('.pokemon-list-element').should('exist')
     })
 })
