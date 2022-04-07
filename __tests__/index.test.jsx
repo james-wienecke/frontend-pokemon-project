9086fe84a13b2cbf86ce2a19fdbaadbb22dd467e
James Wienecke <james.a.wienecke@gmail.com>
2022-04-07 13:35:02 -0500

    Footer and pokemon list tests written. Footer test passing, pokemon list failing.

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -2,11 +2,30 @@ import {render, screen} from '@testing-library/react';
 import Home from '../pages/index';
 
 describe('Home', () => {
+    beforeEach(() => {
+        render(<Home />);
+    })
     it("renders the page heading", () => {
-        render(<Home/>);
+        // render(<Home/>);
 
-        const h1 = screen.getByText("Pokemon Explorer");
+        const h1 = screen.getByText(/pokemon explorer/i);
 
         expect(h1).toBeInTheDocument();
+    })
+
+    it("renders the page footer", () => {
+        // render(<Home />)
+
+        const footer = screen.getByText(/james wienecke/i);
+
+        expect(footer).toBeInTheDocument();
+    })
+
+    it("contains the PokemonList component", () => {
+        // render(<Home />);
+
+        const pokemonList = screen.getByTestId(/pokemon-list-container/);
+
+        expect(pokemonList).toBeInTheDocument();
     })
 })
