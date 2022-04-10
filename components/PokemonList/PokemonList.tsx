0f38e0bcf134a2c76ea6d5b7db54de766856c86f
James Wienecke <james.a.wienecke@gmail.com>
2022-04-09 23:26:02 -0500

    Began work on the code for building a number of pokemon elements.

--- components/PokemonList/PokemonList.tsx
+++ components/PokemonList/PokemonList.tsx
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function PokemonList() {
+
+    const [limit, setLimit] = useState(10);
+
+
+    const createListElements = () => {
+        const elements = [];
+        for (let i = 0; i < limit; i++) {
+            elements.push();
+        }
+    }
+
     return (
         <section data-testid="pokemon-list-container">
             PokemonList
