bcacc0cc7e1947d2bbde648376279e891534d06b
James Wienecke <james.a.wienecke@gmail.com>
2022-04-09 23:43:12 -0500

    Wrote super basic component for displaying one pokemon.

---
+++ components/PokemonElement/PokemonElement.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function PokemonElement() {
+
+    return (
+        <div data-testid="pokemon-item">
+            <p>Pokemon Element</p>
+        </div>
+    );
+}
+
+export default PokemonElement;
