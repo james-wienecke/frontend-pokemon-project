3d5aa4cff5974bdcafe7f56f62f893a788eb8291
James Wienecke <james.a.wienecke@gmail.com>
2022-04-12 23:32:00 -0500

    Fixed issue. Test actually mocks the api call now.

--- __tests__/components/PokemonElement.test.jsx
+++ __tests__/components/PokemonElement.test.jsx
@@ -6,12 +6,9 @@ describe("Pokemon Element component", () => {
     it("gets the proper name for pokemon with id of 1", async () => {
         nock("https://pokeapi.co/api/v2/")
             .get("/pokemon/1")
-            .reply(200, {
-                data: require("../mock_json/1.json")
-            });
+            .reply(200, require("../mock_json/1.json"));
 
         render(<PokemonElement pkmn={0} />);
-
 
         const pokemonElement = await screen.findByText(/bulbasaur/i);
         expect(pokemonElement).toBeInTheDocument();
