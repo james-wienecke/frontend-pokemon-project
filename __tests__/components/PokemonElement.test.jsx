8bc3055dc14b927ca5d0fefc916e84234984514c
James Wienecke <james.a.wienecke@gmail.com>
2022-04-12 23:27:07 -0500

    Fixed very funny issue with nock path definition. New issues

--- __tests__/components/PokemonElement.test.jsx
+++ __tests__/components/PokemonElement.test.jsx
@@ -4,13 +4,14 @@ import nock from 'nock';
 
 describe("Pokemon Element component", () => {
     it("gets the proper name for pokemon with id of 1", async () => {
-        nock("GET https://pokeapi.co/api/v2/")
+        nock("https://pokeapi.co/api/v2/")
             .get("/pokemon/1")
             .reply(200, {
                 data: require("../mock_json/1.json")
             });
 
         render(<PokemonElement pkmn={0} />);
+
 
         const pokemonElement = await screen.findByText(/bulbasaur/i);
         expect(pokemonElement).toBeInTheDocument();
