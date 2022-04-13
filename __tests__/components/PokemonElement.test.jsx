4ca7ea189a4f8631d323f0c7b342e7f20986440b
James Wienecke <james.a.wienecke@gmail.com>
2022-04-12 22:50:59 -0500

    Switched over to using local data via nock for testing.

--- __tests__/components/PokemonElement.test.jsx
+++ __tests__/components/PokemonElement.test.jsx
@@ -7,48 +7,7 @@ describe("Pokemon Element component", () => {
         nock("GET https://pokeapi.co/api/v2/")
             .get("/pokemon/1")
             .reply(200, {
-                data: {
-                    abilities: [],
-                    base_experience: 64,
-                    forms: [
-                        {
-                            name: "bulbasaur",
-                            url: "https://pokeapi.co/api/v2/pokemon-form/1/"
-                        }
-                    ],
-                    game_indices: [],
-                    height: 7,
-                    held_items: [ ],
-                    id: 1,
-                    is_default: true,
-                    location_area_encounters: "https://pokeapi.co/api/v2/pokemon/1/encounters",
-                    moves: [],
-                    name: "bulbasaur",
-                    order: 1,
-                    past_types: [ ],
-                    species: {},
-                    sprites: {
-                        front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
-                    },
-                    stats: [],
-                    types: [
-                        {
-                            slot: 1,
-                            type: {
-                                name: "grass",
-                                url: "https://pokeapi.co/api/v2/type/12/"
-                            }
-                        },
-                        {
-                            slot: 2,
-                            type: {
-                                name: "poison",
-                                url: "https://pokeapi.co/api/v2/type/4/"
-                            }
-                        }
-                    ],
-                    weight: 69
-                }
+                data: require("../mock_json/1.json")
             });
 
         render(<PokemonElement pkmn={0} />);
